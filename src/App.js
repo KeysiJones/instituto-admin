231825bb3e43eddd4482e2e424bf639d6da8e9d8
keysi.fernandes <keysi.fernandes@hypeflame.com.br>
2021-09-06 20:41:06 -0300

    feat: adiciona login e senha para ediçao dos links dos cursos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,71 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 
-function App() {
-  const AULAS_URL =
-    "https://backend-instituto.herokuapp.com/aulas/6128177affc6504f682dbb81";
+const BASE_URL = "https://backend-instituto.herokuapp.com";
+// const BASE_URL = "http://192.168.0.13:3001";
+const AULAS_URL = `${BASE_URL}/aulas/6128177affc6504f682dbb81`;
+const LOGIN_URL = `${BASE_URL}/login`;
+const verifyLogged = (isValid) => {
+  let username, password;
 
+  const token = localStorage.getItem("authToken") || "";
+
+  if (isValid) {
+    while (!username && !token) {
+      username = prompt("Por favor, digite o seu usuário abaixo:");
+    }
+
+    while (!password && !token) {
+      password = prompt("Agora digite a sua senha:");
+    }
+
+    if (username && password) {
+      login(username, password);
+    }
+  } else {
+    while (!username) {
+      username = prompt("Por favor, digite o seu usuário abaixo:");
+    }
+
+    while (!password) {
+      password = prompt("Agora digite a sua senha:");
+    }
+
+    if (username && password) {
+      login(username, password);
+    }
+  }
+};
+
+const login = (username, password) => {
+  fetch(LOGIN_URL, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({ username, password }),
+  })
+    .then((res) => res.json())
+    .then((resposta) => {
+      if (resposta.status === 200 && resposta.token) {
+        localStorage.setItem("authToken", resposta.token);
+        alert("Login realizado com sucesso");
+        return;
+      }
+
+      alert(resposta.message);
+    });
+};
+
+function App() {
   const [aulasTerca, setAulasTerca] = useState([]);
   const [aulasQuarta, setAulasQuarta] = useState([]);
   const [aulasQuinta, setAulasQuinta] = useState([]);
   const [aulasSabado, setAulasSabado] = useState([]);
 
   useEffect(() => {
+    verifyLogged(true);
+
     fetch(AULAS_URL)
       .then((res) => res.json())
       .then((cursos) => {
@@ -45,12 +100,14 @@ const AulasSemana = ({ aulas, diaSemana }) => {
   const [edit, setEdit] = useState(false);
 
   const handleChange = (e) => {
-    setValue({ [e.target.name]: e.target.value });
+    setValue((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
   };
 
   useEffect(() => {
     const cursoArray = [];
-
     aulas.forEach((curso) => {
       cursoArray[`${diaSemana}-horario-${curso.id}`] = curso.horario;
       cursoArray[`${diaSemana}-link-${curso.id}`] = curso.link;
@@ -59,19 +116,25 @@ const AulasSemana = ({ aulas, diaSemana }) => {
 
     setValue(cursoArray);
   }, [aulas]);
+  const dia = {
+    terca: "terça",
+    quarta: "quarta",
+    quinta: "quinta",
+    sabado: "sábado",
+  };
 
   return (
     <div>
-      <p className="my-12">Aulas de {diaSemana}</p>
+      <p className="my-12 text-3xl">Aulas de {dia[diaSemana]}</p>
       <div
-        className="border-2 border-blue-500 m-12 rounded-lg"
+        className="border-4 border-white m-12 rounded-lg"
         style={{ textAlignLast: "center" }}
       >
         <table>
           <thead>
             <tr>
-              <th>Nome</th>
-              <th>Horario</th>
+              <th>Curso</th>
+              <th>Horário</th>
               <th>Link</th>
               <th>Ações</th>
             </tr>
@@ -91,7 +154,9 @@ const AulasSemana = ({ aulas, diaSemana }) => {
                         className="text-black rounded-md p-2"
                       />
                     ) : (
-                      <p>{curso.nome}</p>
+                      <p>
+                        {value[`${diaSemana}-nome-${curso.id}`] ?? curso.nome}
+                      </p>
                     )}
                   </td>
                   <td id={`horario-${diaSemana}-${curso.id}`} className="p-2">
@@ -106,7 +171,10 @@ const AulasSemana = ({ aulas, diaSemana }) => {
                         className="text-black rounded-md p-2 w-32"
                       />
                     ) : (
-                      <p>{curso.horario}</p>
+                      <p>
+                        {value[`${diaSemana}-horario-${curso.id}`] ??
+                          curso.horario}
+                      </p>
                     )}
                   </td>
                   <td id={`link-${diaSemana}-${curso.id}`} className="p-2">
@@ -120,7 +188,13 @@ const AulasSemana = ({ aulas, diaSemana }) => {
                         className="text-black rounded-md p-2 w-full"
                       />
                     ) : (
-                      <p>{curso.link}</p>
+                      <a
+                        href={
+                          value[`${diaSemana}-link-${curso.id}`] ?? curso.link
+                        }
+                      >
+                        {value[`${diaSemana}-link-${curso.id}`] ?? curso.link}
+                      </a>
                     )}
                   </td>
                   <td className="p-2">
@@ -128,6 +202,56 @@ const AulasSemana = ({ aulas, diaSemana }) => {
                       onClick={(e) => {
                         setEdit(!edit);
                         setEditableRow(`${diaSemana}-${curso.id}`);
+                        if (e.target.innerText === "Salvar") {
+                          if (
+                            // eslint-disable-next-line no-restricted-globals
+                            confirm("Deseja salvar as alterações realizadas ?")
+                          ) {
+                            let novasAulas = {
+                              id: curso.id,
+                              link: value[`${diaSemana}-link-${curso.id}`],
+                              nome: value[`${diaSemana}-nome-${curso.id}`],
+                              horario:
+                                value[`${diaSemana}-horario-${curso.id}`],
+                            };
+
+                            let updatedData = {
+                              diaSemana: diaSemana,
+                              novasAulas: novasAulas,
+                            };
+
+                            const authToken =
+                              localStorage.getItem("authToken") || "";
+
+                            fetch(AULAS_URL, {
+                              headers: {
+                                "Content-Type": "application/json",
+                                "x-access-token": authToken,
+                              },
+                              method: "PUT",
+                              body: JSON.stringify(updatedData),
+                            })
+                              .then((res) => {
+                                if (res.status === 403 || res.status === 401) {
+                                  alert(
+                                    "Você precisa estar logado para editar registros, por favor digite o seu usuário e senha."
+                                  );
+                                  verifyLogged(false);
+                                }
+
+                                if (res.status === 200) {
+                                  alert("Registro atualizado com sucesso");
+                                }
+                              })
+                              .then((res) => {
+                                if (res.ok === 1) {
+                                  alert("Registro atualizado com sucesso");
+                                }
+                                alert("Erro ao atualizar registro");
+                              })
+                              .catch((err) => console.log(err));
+                          }
+                        }
                       }}
                       className="p-4 bg-blue-400 rounded-2xl m-2"
                     >
